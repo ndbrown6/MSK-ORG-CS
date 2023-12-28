1764c42eea372dacde97998c4d31d33a39628a33
David Brown <ndbrown6@gmail.com>
2023-12-28 14:36:04 -0500

    ++

--- R/00_barplot_and_heatmap.R
+++ R/00_barplot_and_heatmap.R
@@ -15,14 +15,15 @@ manifest = readr::read_tsv(file = url_manifest, col_names = TRUE, col_types = co
 			 sarcoma_classification = `sarcoma component classification (1=homologous; 2=heterologous-including rhabdo, chondroid, chondrosarc)`,
 			 `sarcoma_predominant_%` = `sarcoma predominant? >50%`,
 			 sarcoma_predominant_yes_no = `sarcoma predominant? (1=yes, 2=no)`,
-			 her2_amplification_rep_1 = `HER-2 gene amplification`,
-			 her2_amplification_rep_2 = X12,
-			 her2_amplification_rep_3 = X13,
-			 her2_amplification_average = `HER-2 amplification avg`,
-			 trop2_amplification_rep_1 = `TROP-2 amplification`,
-			 trop2_amplification_rep_2 = X16,
-			 trop2_amplification_rep_3 = X17,
-			 trop2_amplification_average = `TROP-2 amplification average`) %>%
+			 HER2_gene_expression_1,
+			 HER2_gene_expression_2,
+			 HER2_gene_expression_3,
+			 HER2_gene_expression_mean,
+			 TROP2_gene_expression_1,
+			 TROP2_gene_expression_2,
+			 TROP2_gene_expression_3,
+			 TROP2_gene_expression_mean,
+			 ERBB2_gene_amplification) %>%
 	   dplyr::mutate(is_tp53_mutated_yes_no = case_when(
 		   is_tp53_mutated_yes_no == "Yes" ~ "yes",
 		   is_tp53_mutated_yes_no == "No" ~ "no"
@@ -62,20 +63,27 @@ manifest = readr::read_tsv(file = url_manifest, col_names = TRUE, col_types = co
 		   sarcoma_predominant_yes_no == "1" ~ "yes",
 		   sarcoma_predominant_yes_no == "2" ~ "no"
 	   )) %>%
-	   dplyr::mutate(trop2_amplification_average = ifelse(trop2_amplification_average == "#DIV/0!", NA, trop2_amplification_average)) %>%
 	   readr::type_convert()
-	   
 
 plot_ = manifest %>%
+	reshape2::melt(id.vars = c("sample_name", "ERBB2_gene_amplification"), measure.vars = c("HER2_gene_expression_1", "HER2_gene_expression_2", "HER2_gene_expression_3")) %>%
 	dplyr::group_by(sample_name) %>%
-	dplyr::summarize(her2_amplification_average = her2_amplification_average,
-			 her2_amplification_min = min(her2_amplification_rep_1, her2_amplification_rep_2, her2_amplification_rep_3, na.rm=TRUE),
-			 her2_amplification_max = max(her2_amplification_rep_1, her2_amplification_rep_2, her2_amplification_rep_3, na.rm=TRUE)) %>%
-	dplyr::arrange(desc(her2_amplification_average)) %>%
+	dplyr::summarize(ERBB2_gene_amplification = unique(ERBB2_gene_amplification),
+			 mean = mean(value, na.rm = TRUE),
+			 min = min(value, na.rm = TRUE),
+			 max = max(value, na.rm = TRUE)) %>%
+	dplyr::ungroup() %>%
+	dplyr::mutate(sample_name = gsub(pattern = "cs", replacement = "CS", x = sample_name, fixed = TRUE)) %>%
+	dplyr::mutate(ERBB2_gene_amplification = case_when(
+		is.na(ERBB2_gene_amplification) ~ "grey",
+		TRUE ~ ERBB2_gene_amplification
+	)) %>%
+	dplyr::arrange(desc(mean)) %>%
 	dplyr::mutate(sample_name = factor(sample_name, levels = unique(sample_name), ordered = TRUE)) %>%
-	ggplot(aes(x = sample_name, y = her2_amplification_average, ymin = her2_amplification_min, ymax = her2_amplification_max)) +
+	ggplot(aes(x = sample_name, y = mean, ymin = min, ymax = max, fill = ERBB2_gene_amplification)) +
 	geom_bar(stat = "identity", width = .85) +
-	geom_pointrange(stat = "identity", shape = 21, fill = "white", color = "grey10", size = .65) +
+	geom_pointrange(stat = "identity", shape = 21, fill = "white", color = "grey10", size = .6) +
+	scale_fill_brewer(type = "qual", palette = 2) +
 	scale_y_sqrt(limits = c(0, .81),
 		     breaks = c(seq(from = .01, to = .09, by = .01), seq(from = .1, to = .9, by = .1)),
 		     labels = c(c("", ".02", "", ".04", "", ".06", "", ".08", ""), seq(from = .1, to = .9, by = .1))) +
@@ -85,8 +93,39 @@ plot_ = manifest %>%
 	theme(axis.title.x = element_text(margin = margin(t = 20), size = 14),
  	      axis.title.y = element_text(margin = margin(r = 20), size = 14),
 	      axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size = 12),
-	      axis.text.y = element_text(size = 12))
+	      axis.text.y = element_text(size = 12)) +
+	guides(fill = guide_legend(title = "ERBB2\nAmplification"))
 
-pdf(file = "../res/Fold_change_by_Sample.pdf", width = 14, height = 6)
+pdf(file = "../res/Fold_change_by_Sample_Her2.pdf", width = 14, height = 6)
 print(plot_)
 dev.off()
+
+plot_ = manifest %>%
+	reshape2::melt(id.vars = "sample_name", measure.vars = c("TROP2_gene_expression_1", "TROP2_gene_expression_2", "TROP2_gene_expression_3")) %>%
+	dplyr::group_by(sample_name) %>%
+	dplyr::summarize(mean = mean(value, na.rm = TRUE),
+			 min = min(value, na.rm = TRUE),
+			 max = max(value, na.rm = TRUE)) %>%
+	dplyr::ungroup() %>%
+	tidyr::drop_na() %>%
+	dplyr::mutate(sample_name = gsub(pattern = "cs", replacement = "CS", x = sample_name, fixed = TRUE)) %>%
+	dplyr::arrange(desc(mean)) %>%
+	dplyr::mutate(sample_name = factor(sample_name, levels = unique(sample_name), ordered = TRUE)) %>%
+	ggplot(aes(x = sample_name, y = mean, ymin = min, ymax = max)) +
+	geom_bar(stat = "identity", width = .85) +
+	geom_pointrange(stat = "identity", shape = 21, fill = "white", color = "grey10", size = .6) +
+	scale_y_sqrt(limits = c(0, 150),
+		     breaks = c(seq(from = 1, to = 7, by = 2), seq(from = 10, to = 150, by = 20)),
+		     labels = c(seq(from = 1, to = 7, by = 2), seq(from = 10, to = 150, by = 20))) +
+	xlab("") +
+	ylab("Expression Fold-Change") +
+	theme_minimal() +
+	theme(axis.title.x = element_text(margin = margin(t = 20), size = 14),
+ 	      axis.title.y = element_text(margin = margin(r = 20), size = 14),
+	      axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size = 12),
+	      axis.text.y = element_text(size = 12))
+
+pdf(file = "../res/Fold_change_by_Sample_Trop2.pdf", width = 14, height = 6)
+print(plot_)
+dev.off()
+

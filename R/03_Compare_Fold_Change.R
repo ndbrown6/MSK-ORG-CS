9e7e529af826c1102cc74340ef6b85625af32a10
David Brown <ndbrown6@gmail.com>
2024-01-20 16:57:12 -0500

    ++

--- R/03_Compare_Fold_Change.R
+++ R/03_Compare_Fold_Change.R
@@ -47,9 +47,18 @@ smry_ = manifest %>%
 	
 plot_ = smry_ %>%
         dplyr::mutate(sarcoma_predominant = factor(sarcoma_predominant, levels = c(1,2), ordered = TRUE)) %>%
+	dplyr::mutate(carcinoma_classification = case_when(
+		carcinoma_classification == 1 ~ "Serous",
+		carcinoma_classification == 2 ~ "Endometrioid",
+		carcinoma_classification == 3 ~ "HGNOS",
+		carcinoma_classification == 4 ~ "Undiff",
+	)) %>%
+        dplyr::mutate(carcinoma_classification = factor(carcinoma_classification, levels = c("Serous", "Endometrioid", "HGNOS", "Undiff", "NA"), ordered = TRUE)) %>%
 	ggplot(aes(x = sarcoma_predominant, y = her2)) +
 	geom_boxplot(stat = "boxplot", outlier.shape = NA, fill = "white", show.legend = FALSE) +
-	geom_jitter(position = position_jitter(0.15, 0), size = 3.5, shape = 21, fill = "white", alpha = .85, inherit.aes = TRUE) +
+	geom_jitter(mapping = aes(x = sarcoma_classification, y = her2, color = carcinoma_classification),
+		    position = position_jitter(0.15, 0), size = 3.5, shape = 21, fill = "white", alpha = .75, inherit.aes = TRUE) +
+	scale_color_brewer(type = "qual", palette = 6) +
 	scale_x_discrete(breaks = c("1", "2"),
 			 labels = c("Yes", "No")) +
 	scale_y_sqrt(limits = c(0, .81),
@@ -59,25 +68,35 @@ plot_ = smry_ %>%
 	ylab("HER2 Expression Fold-Change") +
 	geom_signif(stat = "signif", test = "wilcox.test", test.args = list(exact = FALSE, alternative = "two.sided"),
 		    y_position = sqrt(0.75),
-		    textsize = 2.5,
-		    vjust = -1,
+		    textsize = 3,
+		    vjust = 0,
 		    comparison = list(c("1", "2"))) +
 	theme_minimal() +
 	theme(axis.title.x = element_text(margin = margin(t = 20)),
  	      axis.title.y = element_text(margin = margin(r = 20)),
 	      axis.text.x = element_text(size = 11),
-	      axis.text.y = element_text(size = 11))
+	      axis.text.y = element_text(size = 11)) +
+	guides(color = guide_legend(title = "Carcinoma\nclassification"))
 	
-pdf(file = "../res/Her2_by_Predominat_Sarcoma.pdf", width = 3.5, height = 4)
+pdf(file = "../res/Her2_by_Predominat_Sarcoma.pdf", width = 4.5, height = 4.5)
 print(plot_)
 dev.off()
 
 plot_ = smry_ %>%
 	dplyr::filter(!is.na(sarcoma_classification)) %>%
         dplyr::mutate(sarcoma_classification = factor(sarcoma_classification, levels = c(1,2), ordered = TRUE)) %>%
+	dplyr::mutate(carcinoma_classification = case_when(
+		carcinoma_classification == 1 ~ "Serous",
+		carcinoma_classification == 2 ~ "Endometrioid",
+		carcinoma_classification == 3 ~ "HGNOS",
+		carcinoma_classification == 4 ~ "Undiff"
+	)) %>%
+	dplyr::mutate(carcinoma_classification = factor(carcinoma_classification, levels = c("Serous", "Endometrioid", "HGNOS", "Undiff", "NA"), ordered = TRUE)) %>%
 	ggplot(aes(x = sarcoma_classification, y = her2)) +
 	geom_boxplot(stat = "boxplot", outlier.shape = NA, fill = "white", show.legend = FALSE) +
-	geom_jitter(position = position_jitter(0.15, 0), size = 3.5, shape = 21, fill = "white", alpha = .85, inherit.aes = TRUE) +
+	geom_jitter(mapping = aes(x = sarcoma_classification, y = her2, color = carcinoma_classification),
+		    position = position_jitter(0.15, 0), size = 3.5, shape = 21, fill = "white", alpha = .75, inherit.aes = TRUE) +
+	scale_color_brewer(type = "qual", palette = 6) +
 	scale_x_discrete(breaks = c("1", "2"),
 			 labels = c("Homologous", "Heterologous")) +
 	scale_y_sqrt(limits = c(0, .81),
@@ -87,16 +106,17 @@ plot_ = smry_ %>%
 	ylab("HER2 Expression Fold-Change") +
 	geom_signif(stat = "signif", test = "wilcox.test", test.args = list(exact = FALSE, alternative = "two.sided"),
 		    y_position = sqrt(0.75),
-		    textsize = 2.5,
-		    vjust = -1,
+		    textsize = 3,
+		    vjust = 0,
 		    comparison = list(c("1", "2"))) +
 	theme_minimal() +
 	theme(axis.title.x = element_text(margin = margin(t = 20)),
  	      axis.title.y = element_text(margin = margin(r = 20)),
 	      axis.text.x = element_text(size = 11),
-	      axis.text.y = element_text(size = 11))
+	      axis.text.y = element_text(size = 11)) +
+	guides(color = guide_legend(title = "Carcinoma\nclassification"))
 	
-pdf(file = "../res/Her2_by_Sarcoma_Classification.pdf", width = 3.5, height = 4)
+pdf(file = "../res/Her2_by_Sarcoma_Classification.pdf", width = 4.5, height = 4.5)
 print(plot_)
 dev.off()
 
@@ -114,9 +134,18 @@ smry_ = manifest %>%
 	
 plot_ = smry_ %>%
         dplyr::mutate(sarcoma_predominant = factor(sarcoma_predominant, levels = c(1,2), ordered = TRUE)) %>%
+	dplyr::mutate(carcinoma_classification = case_when(
+		carcinoma_classification == 1 ~ "Serous",
+		carcinoma_classification == 2 ~ "Endometrioid",
+		carcinoma_classification == 3 ~ "HGNOS",
+		carcinoma_classification == 4 ~ "Undiff"
+	)) %>%
+	dplyr::mutate(carcinoma_classification = factor(carcinoma_classification, levels = c("Serous", "Endometrioid", "HGNOS", "Undiff", "NA"), ordered = TRUE)) %>%
 	ggplot(aes(x = sarcoma_predominant, y = trop2)) +
 	geom_boxplot(stat = "boxplot", outlier.shape = NA, fill = "white", show.legend = FALSE) +
-	geom_jitter(position = position_jitter(0.15, 0), size = 3.5, shape = 21, fill = "white", alpha = .85, inherit.aes = TRUE) +
+	geom_jitter(mapping = aes(x = sarcoma_predominant, y = trop2, color = carcinoma_classification),
+		    position = position_jitter(0.15, 0), size = 3.5, shape = 21, fill = "white", alpha = .75, inherit.aes = TRUE) +
+	scale_color_brewer(type = "qual", palette = 6) +
 	scale_x_discrete(breaks = c("1", "2"),
 			 labels = c("Yes", "No")) +
 	scale_y_sqrt(limits = c(0, 150),
@@ -126,25 +155,36 @@ plot_ = smry_ %>%
 	ylab("TROP2 Expression Fold-Change") +
 	geom_signif(stat = "signif", test = "wilcox.test", test.args = list(exact = FALSE, alternative = "two.sided"),
 		    y_position = sqrt(140),
-		    textsize = 2.5,
-		    vjust = -1,
+		    textsize = 3,
+		    vjust = 0,
 		    comparison = list(c("1", "2"))) +
 	theme_minimal() +
 	theme(axis.title.x = element_text(margin = margin(t = 20)),
  	      axis.title.y = element_text(margin = margin(r = 20)),
 	      axis.text.x = element_text(size = 11),
-	      axis.text.y = element_text(size = 11))
+	      axis.text.y = element_text(size = 11)) +
+	guides(color = guide_legend(title = "Carcinoma\nclassification"))
 	
-pdf(file = "../res/Trop2_by_Predominat_Sarcoma.pdf", width = 3.5, height = 4)
+pdf(file = "../res/Trop2_by_Predominat_Sarcoma.pdf", width = 4.5, height = 4.5)
 print(plot_)
 dev.off()
 
 plot_ = smry_ %>%
 	dplyr::filter(!is.na(sarcoma_classification)) %>%
-        dplyr::mutate(sarcoma_classification = factor(sarcoma_classification, levels = c(1,2), ordered = TRUE)) %>%
+	dplyr::mutate(sarcoma_classification = factor(sarcoma_classification, levels = c(1,2), ordered = TRUE)) %>%
+	dplyr::mutate(sarcoma_predominant = factor(sarcoma_predominant, levels = c(1,2), ordered = TRUE)) %>%
+		dplyr::mutate(carcinoma_classification = case_when(
+		carcinoma_classification == 1 ~ "Serous",
+		carcinoma_classification == 2 ~ "Endometrioid",
+		carcinoma_classification == 3 ~ "HGNOS",
+		carcinoma_classification == 4 ~ "Undiff"
+	)) %>%
+	dplyr::mutate(carcinoma_classification = factor(carcinoma_classification, levels = c("Serous", "Endometrioid", "HGNOS", "Undiff", "NA"), ordered = TRUE)) %>%
 	ggplot(aes(x = sarcoma_classification, y = trop2)) +
 	geom_boxplot(stat = "boxplot", outlier.shape = NA, fill = "white", show.legend = FALSE) +
-	geom_jitter(position = position_jitter(0.15, 0), size = 3.5, shape = 21, fill = "white", alpha = .85, inherit.aes = TRUE) +
+	geom_jitter(mapping = aes(x = sarcoma_classification, y = trop2, color = carcinoma_classification),
+		    position = position_jitter(0.15, 0), size = 3.5, shape = 21, fill = "white", alpha = .75, inherit.aes = TRUE) +
+	scale_color_brewer(type = "qual", palette = 6) +
 	scale_x_discrete(breaks = c("1", "2"),
 			 labels = c("Homologous", "Heterologous")) +
 	scale_y_sqrt(limits = c(0, 150),
@@ -154,15 +194,61 @@ plot_ = smry_ %>%
 	ylab("TROP2 Expression Fold-Change") +
 	geom_signif(stat = "signif", test = "wilcox.test", test.args = list(exact = FALSE, alternative = "two.sided"),
 		    y_position = sqrt(140),
-		    textsize = 2.5,
-		    vjust = -1,
+		    textsize = 3,
+		    vjust = 0,
 		    comparison = list(c("1", "2"))) +
 	theme_minimal() +
 	theme(axis.title.x = element_text(margin = margin(t = 20)),
  	      axis.title.y = element_text(margin = margin(r = 20)),
 	      axis.text.x = element_text(size = 11),
-	      axis.text.y = element_text(size = 11))
+	      axis.text.y = element_text(size = 11)) +
+	guides(color = guide_legend(title = "Carcinoma\nclassification"))
 	
-pdf(file = "../res/Trop2_by_Sarcoma_Classification.pdf", width = 3.5, height = 4)
+pdf(file = "../res/Trop2_by_Sarcoma_Classification.pdf", width = 4.5, height = 4.5)
+print(plot_)
+dev.off()
+
+plot_ = smry_ %>%
+	dplyr::filter(!is.na(sarcoma_classification)) %>%
+	dplyr::mutate(sarcoma_classification = factor(sarcoma_classification, levels = c(1,2), ordered = TRUE)) %>%
+	dplyr::mutate(sarcoma_predominant = factor(sarcoma_predominant, levels = c(1,2), ordered = TRUE)) %>%
+		dplyr::mutate(carcinoma_classification = case_when(
+		carcinoma_classification == 1 ~ "Serous",
+		carcinoma_classification == 2 ~ "Endometrioid",
+		carcinoma_classification == 3 ~ "HGNOS",
+		carcinoma_classification == 4 ~ "Undiff"
+	)) %>%
+	dplyr::mutate(carcinoma_classification = factor(carcinoma_classification, levels = c("Serous", "Endometrioid", "HGNOS", "Undiff", "NA"), ordered = TRUE)) %>%
+	ggplot(aes(x = sarcoma_predominant:sarcoma_classification, y = trop2)) +
+	geom_boxplot(stat = "boxplot", outlier.shape = NA, fill = "white", show.legend = FALSE) +
+	geom_jitter(mapping = aes(x = sarcoma_predominant:sarcoma_classification, y = trop2, color = carcinoma_classification),
+		    position = position_jitter(0.15, 0), size = 3.5, shape = 21, fill = "white", alpha = .75, inherit.aes = TRUE) +
+	scale_color_brewer(type = "qual", palette = 6) +
+	scale_x_discrete(breaks = c("1:1", "1:2", "2:1", "2:2"),
+			 labels = c("Pred sarc\nHom sarc", "Pred sarc\nHet sarc",
+				    "Pred car\nHom sarc", "Pred car\nHet sarc")) +
+	scale_y_sqrt(limits = c(0, 300),
+		     breaks = c(seq(from = 1, to = 7, by = 2), seq(from = 10, to = 150, by = 20)),
+		     labels = c(seq(from = 1, to = 7, by = 2), seq(from = 10, to = 150, by = 20))) +
+	xlab("Sarcoma classification") +
+	ylab("TROP2 Expression Fold-Change") +
+	geom_signif(stat = "signif", test = "wilcox.test", test.args = list(exact = FALSE, alternative = "two.sided"),
+		    y_position = sqrt(seq(140, 300, length = 6)),
+		    textsize = 3,
+		    vjust = 0,
+		    comparison = list(c("1:1", "1:2"),
+				      c("1:1", "2:1"),
+				      c("1:1", "2:2"),
+				      c("1:2", "2:1"),
+				      c("1:2", "2:2"),
+				      c("2:1", "2:2"))) +
+	theme_minimal() +
+	theme(axis.title.x = element_text(margin = margin(t = 20)),
+ 	      axis.title.y = element_text(margin = margin(r = 20)),
+	      axis.text.x = element_text(size = 11),
+	      axis.text.y = element_text(size = 11)) +
+	guides(color = guide_legend(title = "Carcinoma\nclassification"))
+	
+pdf(file = "../res/Trop2_by_Predominant_Sarcome,Sarcoma_Classification.pdf", width = 5.5, height = 5.5)
 print(plot_)
 dev.off()

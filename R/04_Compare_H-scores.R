3d8d2be919c816b0f0828980e9eaac149333f84d
David Brown <ndbrown6@gmail.com>
2025-01-07 10:15:34 -0500

    ++

--- R/04_Compare_H-scores.R
+++ R/04_Compare_H-scores.R
@@ -20,14 +20,15 @@ plot_ = smry_ %>%
 	dplyr::mutate(variable = gsub(" H-score", "", variable, fixed = TRUE)) %>%
 	ggplot(aes(x = variable, y = value)) +
 	geom_boxplot(stat = "boxplot", outlier.shape = NA, fill = "white", show.legend = FALSE) +
+	geom_jitter(position = position_jitter(0.1, 0), size = 3.5, shape = 21, fill = "white", alpha = .75, inherit.aes = TRUE) +
 	scale_x_discrete() +
-	scale_y_continuous() +
+	scale_y_continuous(limits = c(0, 300)) +
 	xlab(" ") +
 	ylab("TROP2 H-score") +
 	geom_signif(stat = "signif", test = "wilcox.test", test.args = list(exact = FALSE, alternative = "two.sided"),
-		    y_position = 300,
+		    y_position = 280,
 		    textsize = 3,
-		    vjust = 0,
+		    vjust = -.30,
 		    comparison = list(c("Carcinoma", "Sarcoma"))) +
 	theme_minimal() +
 	theme(axis.title.x = element_text(margin = margin(t = 20)),
@@ -36,36 +37,5 @@ plot_ = smry_ %>%
 	      axis.text.y = element_text(size = 11))
 	
 pdf(file = "../res/Trop2_H-score.pdf", width = 3, height = 4)
-print(plot_)
-dev.off()
-
-plot_ = smry_ %>%
-	dplyr::select(`sample number`, `Carcinoma H-score`, `Sarcoma H-score`) %>%
-	reshape2::melt() %>%
-	dplyr::mutate(variable = gsub(" H-score", "", variable, fixed = TRUE)) %>%
-	ggplot(aes(x = variable, y = value)) +
-	geom_boxplot(stat = "boxplot", outlier.shape = NA, fill = "white", show.legend = FALSE) +
-	geom_path(data = smry_ %>%
-		  	 dplyr::select(`sample number`, `Carcinoma H-score`, `Sarcoma H-score`) %>%
-		  	 reshape2::melt() %>%
-		  	 dplyr::mutate(variable = gsub(" H-score", "", variable, fixed = TRUE)),
-		  mapping = aes(x = variable, y = value, group = `sample number`),
-		  size = .1) +
-	scale_x_discrete() +
-	scale_y_continuous() +
-	xlab(" ") +
-	ylab("TROP2 H-score") +
-	geom_signif(stat = "signif", test = "wilcox.test", test.args = list(exact = FALSE, alternative = "two.sided"),
-		    y_position = 300,
-		    textsize = 3,
-		    vjust = 0,
-		    comparison = list(c("Carcinoma", "Sarcoma"))) +
-	theme_minimal() +
-	theme(axis.title.x = element_text(margin = margin(t = 20)),
- 	      axis.title.y = element_text(margin = margin(r = 20)),
-	      axis.text.x = element_text(size = 11),
-	      axis.text.y = element_text(size = 11))
-	
-pdf(file = "../res/Trop2_H-score_with_lines.pdf", width = 3, height = 4)
 print(plot_)
 dev.off()
